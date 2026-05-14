d9ccac5ccd85220b3222d787068567c32dc1dc4a
mxJosS <jangel2003.kimba4@gmail.com>
2026-05-14 01:16:31 -0600

    feat(dashboard): add admin dashboard and  logotipe

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,15 +3,15 @@
 ])
 
 @if($sidebar)
-    <flux:sidebar.brand name="Laravel Starter Kit" {{ $attributes }}>
+    <flux:sidebar.brand name="NetPulse" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-            <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+            <img src="/assets/img/netpulse.png" alt="NetPulse Logo" class="size-7 object-contain" />
         </x-slot>
     </flux:sidebar.brand>
 @else
-    <flux:brand name="Laravel Starter Kit" {{ $attributes }}>
+    <flux:brand name="NetPulse" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-            <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+            <img src="/assets/img/netpulse.png" alt="NetPulse Logo" class="size-7 object-contain" />
         </x-slot>
     </flux:brand>
 @endif
